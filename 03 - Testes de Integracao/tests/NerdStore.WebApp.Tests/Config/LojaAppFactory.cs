d213b46da0fea154d6f316477e5767210bf8cf71
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-13 10:18:20 -0300

    Configuração da factory responsável por subir o ambiente de testes

--- 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/LojaAppFactory.cs
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/LojaAppFactory.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc.Testing;
 //Microsoft.AspNetCore.App
 namespace NerdStore.WebApp.Tests.Config
 {
+    //WebApplicationFactory -> Trata-se de uma factory responsável por subir uma aplicação web
+    //TStartup -> é uma classe genérica que terá as configurações necessárias
     public class LojaAppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
