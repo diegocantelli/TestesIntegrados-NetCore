08d70d1ac488a41f7b37efd13da5252bc2d5b2e5
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-13 10:12:52 -0300

    Adicionando os pacotes necessários para o teste de integração

---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/LojaAppFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+//necessário instalar os pacotes:
+//Microsoft.AspNetCore.Mvc.Testing
+//Microsoft.AspNetCore.App
+namespace NerdStore.WebApp.Tests.Config
+{
+    public class LojaAppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseStartup<TStartup>();
+            builder.UseEnvironment("Testing");
+        }
+    }
+}
